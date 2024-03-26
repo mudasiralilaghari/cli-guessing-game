6a6da28cb3656b93cb909a07c32c6890333c6502
Mudasir Ali <159126998+mudasiralilaghari@users.noreply.github.com>
2024-03-25 20:27:19 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const random =0 ;
+const answers = await inquirer.prompt([{
+ message:"Please guess a number between 1 - 6 : ",
+ type:"number" ,
+ name:"Guessednumber",
+}]);
+if(answers.Guessednumber=== random){
+  console.log("Congratulations you guessed a right number");
+}else{
+  console.log("You Guessed wrong number");
+}
