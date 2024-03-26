b5633faf9dbd8c01e8c261247836a856052ab405
Mudasir Ali <159126998+mudasiralilaghari@users.noreply.github.com>
2024-03-25 20:45:51 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-const random =0 ;
+const random = Math.floor(Math.random( )*6+1);
 const answers = await inquirer.prompt([{
  message:"Please guess a number between 1 - 6 : ",
  type:"number" ,
@@ -8,6 +8,7 @@ const answers = await inquirer.prompt([{
 }]);
 if(answers.Guessednumber=== random){
   console.log("Congratulations you guessed a right number");
+ console.log("Author: Engineer Mudasir Ali");
 }else{
   console.log("You Guessed wrong number");
 }
